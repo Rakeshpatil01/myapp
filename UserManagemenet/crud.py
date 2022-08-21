d3667c8a68642ad721abfcc8cdd4822ab9a1864b
Rakeshpatil01 <patilrp1996@gmail.com>
2022-08-21 20:16:41 +0530

    done with websocket

--- UserManagemenet/crud.py
+++ UserManagemenet/crud.py
@@ -1,13 +1,16 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from . import models, schemas
+from fastapi import WebSocket, WebSocketDisconnect
+from typing import List
 
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+
 def update_user(db: Session, user_data, user_id: int):
-    db_user =  db.query(models.User).filter(models.User.id == user_id).first()
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
     if not db_user:
         raise HTTPException(status_code=404, detail="user not found")
     for key, value in user_data.items():
@@ -17,13 +20,15 @@ def update_user(db: Session, user_data, user_id: int):
     db.refresh(db_user)
     return db_user
 
+
 def delete_user(db: Session, user_id: int):
-    db_user = db.query(models.User).filter(models.User.id ==user_id).first()
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
     if not db_user:
         raise HTTPException(status_code=404, detail="user not found")
     db.delete(db_user)
     db.commit()
     return {"ok": True}
+
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
@@ -34,8 +39,21 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(email=user.email, password=user.password, username=user.username)
+    db_user = models.User(
+        email=user.email, password=user.password, username=user.username)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
--- frontend/apps/data.py
+++ frontend/apps/data.py
@@ -1,10 +1,26 @@
 import streamlit as st
+import asyncio
+from utils import consumer_airquality
+
 
 def app():
-    st.title('Data')
 
-    st.write("This is the `Data` page of the multi-page app.")
+    status = st.empty()
+    connect = st.checkbox("Connect to WS Server")
 
-    st.write("The following is the DataFrame of the `iris` dataset.")
+    selected_visualizations = st.multiselect(
+        "Select Visualizations", ["raw", "graph", "map"], default=["raw"]
+    )
 
+    columns = [col.empty() for col in st.columns(3)]
 
+    window_size = st.number_input("Window Size", min_value=10, max_value=100)
+
+    if connect:
+        asyncio.run(
+            consumer_airquality(
+                dict(zip(selected_visualizations, columns)), window_size, status
+            )
+        )
+    else:
+        status.subheader(f"Disconnected.")
